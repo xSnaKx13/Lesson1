836ac5605bc6f8b63936970005d7ecb993b01873
xSnaKx13 <152309157+xSnaKx13@users.noreply.github.com>
2024-12-13 18:45:53 +0300

    Add files via upload

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите вашу фамилию: ");
+            var lastName = Console.ReadLine();
+            Console.Write("Введите ваши имя: ");
+            var firstName = Console.ReadLine();
+            Console.Write("Введите ваше отчество: ");
+            var midName = Console.ReadLine();
+            Console.Write("Введите дату вашего рождения: ");
+            var birthdayDate = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"Привет, меня зовут {lastName} {firstName} {midName}\nДата рождения {birthdayDate}");
+            Console.ReadKey();
+        }
+    }
+}
